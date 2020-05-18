f1e6b37555bd6e86a166ae7f0921838ae909cc27
Patrick James <pj997067@gmail.com>
2020-05-18 13:35:35 -0400

    Adding sample project and MsSql provider project

---
+++ src/AutoPocoIO.MsSql/DynamicSchema/Services/CrossDb/SqlServerQuerySqlGenerator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Sql;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace AutoPocoIO.DynamicSchema.Services.CrossDb
+{
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+    internal class SqlServerQuerySqlGenerator : Microsoft.EntityFrameworkCore.SqlServer.Query.Sql.Internal.SqlServerQuerySqlGenerator
+    {
+        public SqlServerQuerySqlGenerator(
+            QuerySqlGeneratorDependencies dependencies,
+            Microsoft.EntityFrameworkCore.Query.Expressions.SelectExpression selectExpression,
+           bool rowNumberPagingEnabled)
+           : base(dependencies, selectExpression, rowNumberPagingEnabled)
+        {
+        }
+
+        public override Expression VisitTable(Microsoft.EntityFrameworkCore.Query.Expressions.TableExpression tableExpression)
+        {
+            if (tableExpression is TableExpression tableExpressionWithDb)
+                Sql.Append(SqlGenerator.DelimitIdentifier(tableExpressionWithDb.DatabaseName) + ".");
+
+            Sql.Append(SqlGenerator.DelimitIdentifier(tableExpression.Table, tableExpression.Schema))
+                .Append(AliasSeparator)
+                .Append(SqlGenerator.DelimitIdentifier(tableExpression.Alias));
+
+            return tableExpression;
+        }
+    }
+}
---
+++ src/AutoPocoIO.MsSql/DynamicSchema/Utils/MsSqlDbTypeMapper.cs
@@ -0,0 +1,41 @@
+﻿using AutoPocoIO.Constants;
+using AutoPocoIO.DynamicSchema.Models;
+using AutoPocoIO.DynamicSchema.Util;
+using NetTopologySuite.Geometries;
+using System;
+
+namespace AutoPocoIO.MsSql.DynamicSchema.Utils
+{
+    public class MsSqlDbTypeMapper : DbTypeMapper, IDbTypeMapper
+    {
+        public override DataType DBTypeToDataType(Column column)
+        {
+            string dataType = column.ColumnType.ToUpperInvariant();
+            DataType ret = new DataType
+            {
+                DbType = column.ColumnType
+            };
+
+
+            if (column.ColumnIsNullable)
+            {
+                ret.DbType += "(nullable)";
+            }
+
+            switch (dataType.ToUpperInvariant())
+            {
+                case "HIERARCHYID":
+                    throw new ArgumentException(ExceptionMessages.HierarchyIdNotSupported, nameof(column));
+                case "GEOMETRY":
+                case "GEOGRAPHY":
+                    ret.SystemType = typeof(Geometry);
+                    break;
+                default:
+                    ret = base.DBTypeToDataType(column);
+                    break;
+            }
+
+            return ret;
+        }
+    }
+}
