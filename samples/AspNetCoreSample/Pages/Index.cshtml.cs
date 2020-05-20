22c39e2995e50bb58574a6e0d95d4c3ed32492b3
Patrick James <pj997067@gmail.com>
2020-05-20 12:06:43 -0400

    Add sample data

--- samples/AspNetCoreSample/Pages/Index.cshtml.cs
+++ samples/AspNetCoreSample/Pages/Index.cshtml.cs
@@ -1,17 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreSample.ViewModels;
+using AutoPocoIO.Api;
+using AutoPocoIO.Exceptions;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
 
 namespace AutoPocoIO.Sample.AspNetCore.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ITableOperations _tableOperations;
+
+        public IndexModel(ITableOperations tableOperations)
+        {
+            _tableOperations = tableOperations;
+        }
+
+        public IEnumerable<OrdersViewModel> Orders { get; private set; }
+
+
         public void OnGet()
         {
-
+            try
+            {
+                Orders = _tableOperations.GetAll<OrdersViewModel>("sampleSales", "orders");
+            }
+            catch (ConnectorNotFoundException)
+            {
+                ViewData["ConnectorNeeded"] = "Connector sampleSales not set up.  Go to the dashboard to set configure.";
+                Orders = Array.Empty<OrdersViewModel>();
+            }
         }
     }
 }
