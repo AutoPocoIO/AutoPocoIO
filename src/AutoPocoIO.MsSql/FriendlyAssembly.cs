9d2c13e3e1fdf275311048a2579c79cb02d60aab
Patrick James <pj997067@gmail.com>
2020-09-03 13:26:24 -0400

    Remove ms sql from main project

--- src/AutoPocoIO.MsSql/FriendlyAssembly.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("AutoPocoIO.MsSql.test")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
--- tests/AutoPocoIO.MsSql.test/TestCategories.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AutoPocoIO.test
-{
-    public static class TestCategories
-    {
-        public const string SkipLive = "SkipWhenLiveUnitTesting";
-        public const string Integration = "Integration";
-        public const string Unit = "Unit";
-    }
-}
