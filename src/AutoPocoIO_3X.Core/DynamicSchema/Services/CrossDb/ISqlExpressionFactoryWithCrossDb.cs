6a74c9d7bd8259d7781e2ccc0f7b2b1f6e1d7b56
Patrick James <pj997067@gmail.com>
2020-10-14 16:30:31 -0400

    Add translate overrides for selectexpression

--- src/AutoPocoIO_3X.Core/DynamicSchema/Services/CrossDb/ISqlExpressionFactoryWithCrossDb.cs
+++ src/AutoPocoIO_3X.Core/DynamicSchema/Services/CrossDb/ISqlExpressionFactoryWithCrossDb.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 
 namespace AutoPocoIO.DynamicSchema.Services.CrossDb
 {
     internal interface ISqlExpressionFactoryWithCrossDb : Microsoft.EntityFrameworkCore.Query.ISqlExpressionFactory
     {
         SelectExpression SelectWithCrossDb(IEntityType entityType);
+        ExistsExpression Exists(SelectExpression subquery, bool negated);
+        InExpression In(SqlExpression item, SelectExpression subquery, bool negated);
     }
 }
---
+++ src/AutoPocoIO_3X.Core/DynamicSchema/Services/CrossDb/RelationalProjectionBindingExpressionVisitor.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace AutoPocoIO.DynamicSchema.Services.CrossDb
+{
+    /// <summary>
+    /// Cross db override
+    /// </summary>
+    internal class RelationalProjectionBindingExpressionVisitor : Microsoft.EntityFrameworkCore.Query.Internal.RelationalProjectionBindingExpressionVisitor
+    {
+        private readonly RelationalQueryableMethodTranslatingExpressionVisitor _queryableMethodTranslatingExpressionVisitor;
+        private readonly RelationalSqlTranslatingExpressionVisitor _sqlTranslator;
+
+
+        private SelectExpression _selectExpression;
+        private bool _clientEval;
+
+
+        private readonly IDictionary<ProjectionMember, Expression> _projectionMapping
+            = new Dictionary<ProjectionMember, Expression>();
+        private readonly Stack<ProjectionMember> _projectionMembers = new Stack<ProjectionMember>();
+
+        public RelationalProjectionBindingExpressionVisitor(
+            Microsoft.EntityFrameworkCore.Query.RelationalQueryableMethodTranslatingExpressionVisitor queryableMethodTranslatingExpressionVisitor,
+            RelationalSqlTranslatingExpressionVisitor sqlTranslatingExpressionVisitor)
+            : base(queryableMethodTranslatingExpressionVisitor, sqlTranslatingExpressionVisitor)
+        {
+        }
+
+        public virtual Expression Translate(SelectExpression selectExpression, Expression expression)
+        {
+            _selectExpression = selectExpression;
+            _clientEval = false;
+
+            _projectionMembers.Push(new ProjectionMember());
+
+            var expandedExpression = _queryableMethodTranslatingExpressionVisitor.ExpandWeakEntities(_selectExpression, expression);
+            var result = Visit(expandedExpression);
+
+            if (result == null)
+            {
+                _clientEval = true;
+
+                expandedExpression = _queryableMethodTranslatingExpressionVisitor.ExpandWeakEntities(_selectExpression, expression);
+                result = Visit(expandedExpression);
+
+                _projectionMapping.Clear();
+            }
+
+            _selectExpression.ReplaceProjectionMapping(_projectionMapping);
+            _selectExpression = null;
+            _projectionMembers.Clear();
+            _projectionMapping.Clear();
+
+            return result;
+        }
+    }
+}
